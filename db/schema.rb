9165d5171b55d17a2c2a3216a5928f7ee20cce85
Bryan Larsen <bryan@larsen.st>
2010-02-19 15:38:46 -0500

    migrate-project-memberships
    Run the migration generator to have the required foreign keys added to
    the database:
    
        $ ./script/generate hobo_migration

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091201135235) do
+ActiveRecord::Schema.define(:version => 20091201135345) do
+
+  create_table "project_memberships", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "project_id"
+    t.integer  "user_id"
+  end
+
+  add_index "project_memberships", ["project_id"], :name => "index_project_memberships_on_project_id"
+  add_index "project_memberships", ["user_id"], :name => "index_project_memberships_on_user_id"
 
   create_table "projects", :force => true do |t|
     t.string   "name"
