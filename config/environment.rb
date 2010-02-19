ed1eb001b62fc5508e4ce9359201bcff7f500e41
Bryan Larsen <bryan@larsen.st>
2010-02-19 15:38:40 -0500

    add-bluecloth-gem
    You may need to install the relevant ruby gem: either BlueCloth (markdown) or RedCloth (textile)
    
    SHOW_PATCH

--- config/environment.rb
+++ config/environment.rb
@@ -9,6 +9,8 @@ require File.join(File.dirname(__FILE__), 'boot')
 Rails::Initializer.run do |config|
   config.gem 'hobo'
 
+  config.gem 'bluecloth'
+
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
